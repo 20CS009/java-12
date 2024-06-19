a44256abe94d2cc56603947a30bc8506486e6a1d
avina <avina@LAPTOP-8OGUAHJ5>
2024-06-19 13:51:59 +0530

    learn about java boilerplate code

---
+++ fundamentals/src/in/basics/First_program.java
@@ -0,0 +1,12 @@
+package in.basics;
+
+public class First_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("First program");
+		System.out.println("First Program");
+
+	}
+
+}
